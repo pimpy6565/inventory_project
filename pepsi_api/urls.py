a1768d463efb7468e951076c27d1560eb8820eaf
christopher <chris@christophers-MacBook-Pro.local>
2026-04-05 07:31:20 -0400

    fix

--- pepsi_api/urls.py
+++ pepsi_api/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .serializers import Flavor_view
+
+router = DefaultRouter()
+router.register("flavor",Flavor_view,basename="flavor")
 urlpatterns = [
     path("",views.data,name='data'),
     path("id/<int:id>",views.getid,name="getid"),
     path("update/<int:id>",views.update_async,name='updateapi'),
     path("add/<str:flavor>/<int:amount>",views.add_one,name='add'),
-    path("delete",views.delete,name="deletes")
+    path("delete",views.delete,name="deletes"),
+    path("menu",include(router.urls))
 ]
--- pepsi_ui/urls.py
+++ pepsi_ui/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+
+router = DefaultRouter()
+
+
 urlpatterns = [
     path("",views.main,name="main"),
     path("add",views.add_page,name="add"),
     path("update/<int:id>",views.update,name="update"),
     path("form",views.update_form,name="update_form"),
-    path("delete",views.delete_flavor,name='delete_flavor')
+    path("delete",views.delete_flavor,name='delete_flavor'),
+
 ]
